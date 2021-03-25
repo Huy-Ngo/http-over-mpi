db2b513088c20ec8c9689afd2b49e3d554469868
Ngô Ngọc Đức Huy <huyngo@disroot.org>
2021-03-25 16:50:31 +0700

    Format code

--- proxy.go
+++ proxy.go
@@ -1,31 +1,31 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
-    "io/ioutil"
-    "strings"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"strings"
 )
 
 var Host = "usth.edu.vn/"
 
 func root(w http.ResponseWriter, req *http.Request) {
-    path := strings.TrimPrefix(req.URL.Path, "/")
-    url := Host + path
-    resp, err := http.Get("https://" + url)
-    mimeType := resp.Header.Get("Content-Type")
-    fmt.Println(resp.Status)
-    defer resp.Body.Close()
-    body, err := ioutil.ReadAll(resp.Body)
-    if err != nil {
-        panic(err)
-    }
-    w.Header().Set("Content-Type", mimeType)
-    fmt.Fprintf(w, string(body))
+	path := strings.TrimPrefix(req.URL.Path, "/")
+	url := Host + path
+	resp, err := http.Get("https://" + url)
+	mimeType := resp.Header.Get("Content-Type")
+	fmt.Println(resp.Status)
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+	w.Header().Set("Content-Type", mimeType)
+	fmt.Fprintf(w, string(body))
 }
 
 func main() {
-    http.HandleFunc("/", root)
+	http.HandleFunc("/", root)
 
-    http.ListenAndServe(":8090", nil)
+	http.ListenAndServe(":8090", nil)
 }
