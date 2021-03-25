1e9a7a5e8f7828948d26b74d0b69263b21ee4e4a
Nguyễn Gia Phong <mcsinyx@disroot.org>
2021-03-25 17:44:20 +0700

    Implement MPI proxy

--- proxy.py
+++ proxy.py
@@ -1,34 +1,42 @@
 from sys import argv
+
 from flask import Flask, request, redirect, Response
+from mpi4py import MPI
 import requests
+from requests import Request, Session
 
-app = Flask(__name__, static_folder=None)
+comm = MPI.COMM_WORLD
+rank = comm.Get_rank()
+if rank == 0:
+    if len(argv) != 2:
+        print(f'usage: {argv[0]} hostname')
+        exit()
+    hostname = argv[1]
+    app = Flask(__name__, static_folder=None)
 
-if len(argv) != 2:
-    print(f'usage: {argv[0]} hostname')
-    exit()
-hostname = argv[1]
+    @app.route('/', defaults={'path': ''})
+    @app.route('/<path:path>', methods=['GET', 'OPTIONS', 'HEAD',
+                                        'POST', 'PUT', 'PATCH', 'DELETE'])
+    def proxy(path):
+        r = Request(
+            method=request.method,
+            url=request.url.replace(request.host_url, hostname),
+            headers={key: value for (key, value) in request.headers
+                     if key != 'Host'},
+            data=request.get_data(),
+            cookies=request.cookies).prepare()
+        comm.send(r, dest=1)
+        resp = comm.recv()
+        excluded_headers = ['content-encoding', 'content-length',
+                            'transfer-encoding', 'connection']
+        headers = [(name, value) for (name, value) in resp.headers.items()
+                   if name.lower() not in excluded_headers]
 
-@app.route('/', defaults={'path': ''})
-@app.route('/<path:path>', methods=['GET', 'OPTIONS', 'HEAD',
-                                    'POST', 'PUT', 'PATCH', 'DELETE'])
-def proxy(path):
-    resp = requests.request(
-        method=request.method,
-        url=request.url.replace(request.host_url, hostname),
-        headers={key: value for (key, value) in request.headers
-                 if key != 'Host'},
-        data=request.get_data(),
-        cookies=request.cookies,
-        allow_redirects=False)
+        response = Response(resp.content, resp.status_code, headers)
+        return response
 
-    excluded_headers = ['content-encoding', 'content-length',
-                        'transfer-encoding', 'connection']
-    headers = [(name, value) for (name, value) in resp.raw.headers.items()
-               if name.lower() not in excluded_headers]
-
-    response = Response(resp.content, resp.status_code, headers)
-    return response
-
-if __name__ == '__main__':
     app.run(host='127.0.0.1', port=42069)
+else:
+    session = Session()
+    while True:
+        comm.send(session.send(comm.recv()), dest=0)
