b5138ed4f5bf47113661fffedf99b4ff8b0f932b
Nguyễn Gia Phong <mcsinyx@disroot.org>
2021-03-25 16:08:01 +0700

    Generalize the proxy

--- proxy.py
+++ proxy.py
@@ -1,24 +1,27 @@
 from flask import Flask, request, redirect, Response
 import requests
 
-SITE_NAME = 'https://usth.edu.vn/'
-app = Flask(__name__)
+SITE_NAME = 'https://acanban.ga/'
+app = Flask(__name__, static_folder=None)
 
 
 @app.route('/', defaults={'path': ''})
-@app.route('/<path:path>', methods=['GET','POST'])
+@app.route('/<path:path>', methods=['GET', 'OPTIONS', 'HEAD',
+                                    'POST', 'PUT', 'PATCH', 'DELETE'])
 def proxy(path):
-    print(path)
-    global SITE_NAME
-    if request.method == 'GET':
-        resp = requests.get(f'{SITE_NAME}{path}')
-        excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
-        headers = [(name, value) for (name, value) in  resp.raw.headers.items() if name.lower() not in excluded_headers]
-        response = Response(resp.content, resp.status_code, headers)
-        return response
-    elif request.method == 'POST':
-        resp = requests.post(f'{SITE_NAME}{path}', json=request.get_json())
-        excluded_headers = ['content-encoding', 'content-length', 'transfer-encoding', 'connection']
-        headers = [(name, value) for (name, value) in resp.raw.headers.items() if name.lower() not in excluded_headers]
-        response = Response(resp.content, resp.status_code, headers)
-        return response
+    resp = requests.request(
+        method=request.method,
+        url=request.url.replace(request.host_url, SITE_NAME),
+        headers={key: value for (key, value) in request.headers
+                 if key != 'Host'},
+        data=request.get_data(),
+        cookies=request.cookies,
+        allow_redirects=False)
+
+    excluded_headers = ['content-encoding', 'content-length',
+                        'transfer-encoding', 'connection']
+    headers = [(name, value) for (name, value) in resp.raw.headers.items()
+               if name.lower() not in excluded_headers]
+
+    response = Response(resp.content, resp.status_code, headers)
+    return response
